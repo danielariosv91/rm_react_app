5ff06c31af7eda3473aaab9257c2aae9995f9410
driosv <daniela.rios.v91@gmail.com>
2025-05-11 22:57:16 -0400

    create component character detail

---
+++ src/features/episodes/components/EpisodesDetail.jsx
@@ -0,0 +1,30 @@
+function EpisodeDetail({ character }) {
+    return (
+        <div className="w-84 flex-auto px-7">
+            <div className="w-100 flex-auto ">
+                <div>
+                    <span className="mb-1">Episodes</span>
+                </div>
+
+                <div className="stats">
+                    <div className="stat">
+                        <div className="stat-title">Total Episodes Appearence</div>
+                        <div className="stat-value">{character?.episode.length}</div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-100 flex-auto">
+                {character?.episode.map((item) => (
+                    <div className="avatar avatar-placeholder mx-0.5 my-0.5">
+                        <div className="bg-neutral text-neutral-content w-8 rounded-full">
+                            {/* Open the modal using document.getElementById('ID').showModal() method */}
+                            <button className="btn" onClick={(e) => handleModal(e, item.ep)}>{item.ep}</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default EpisodeDetail;
