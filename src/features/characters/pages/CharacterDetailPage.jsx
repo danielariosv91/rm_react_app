8423b66ebdaf34831d99ce9a19242fcbd24d8ad4
driosv <daniela.rios.v91@gmail.com>
2025-05-11 22:59:37 -0400

    module component detail

--- src/features/characters/pages/CharacterDetailPage.jsx
+++ src/features/characters/pages/CharacterDetailPage.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom"
 import { getOneCharacter } from "../service/charactersService"
 import { getOneEpisode } from "./../../../service/episodesService"
 
+/** components */
+import EpisodeDetail from './../../episodes/components/EpisodesDetail'
+
 function CharacterDetail() {
     const { id } = useParams()
     const [modalContent, setModalContent] = useState()
@@ -76,30 +79,7 @@ function CharacterDetail() {
                             </div>
 
                             {/* TODO: Module this section */}
-                            <div className="w-84 flex-auto px-7">
-                                <div className="w-100 flex-auto ">
-                                    <div>
-                                        <span className="mb-1">Episodes</span>
-                                    </div>
-
-                                    <div className="stats">
-                                        <div className="stat">
-                                            <div className="stat-title">Total Episodes Appearence</div>
-                                            <div className="stat-value">{character?.episode.length}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="w-100 flex-auto">
-                                    {character?.episode.map((item) => (
-                                        <div className="avatar avatar-placeholder mx-0.5 my-0.5">
-                                            <div className="bg-neutral text-neutral-content w-8 rounded-full">
-                                                {/* Open the modal using document.getElementById('ID').showModal() method */}
-                                                <button className="btn" onClick={(e) => handleModal(e, item.ep)}>{item.ep}</button>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
+                            <EpisodeDetail character={character} />
 
 
                             {/* TODO: Module this modal */}
