ebc4ee381e284171d9baaac9c2e1b6c572a22d52
driosv <daniela.rios.v91@gmail.com>
2025-05-12 18:22:19 -0400

    add detail navigation

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -17,7 +17,7 @@ export default function CardItem({ item }) {
                 <h2 className="card-title">{item.name}</h2>
                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                 <div className="card-actions justify-end mt-4">
-                    <button className="btn btn-success" onClick={() => handleNavigate}>Detail</button>
+                    <button className="btn btn-success" onClick={(e) => handleNavigate(e, item.id)}>Detail</button>
                 </div>
             </div>
         </div>
