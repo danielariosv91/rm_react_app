aca7cd79619b3fceb76e559d62794d212ad72393
driosv <daniela.rios.v91@gmail.com>
2025-05-11 22:54:13 -0400

    detail modal view

--- src/features/characters/pages/CharacterDetailPage.jsx
+++ src/features/characters/pages/CharacterDetailPage.jsx
@@ -102,7 +102,21 @@ function CharacterDetail() {
                             </div>
 
 
-                           
+                            {/* TODO: Module this modal */}
+                            <dialog id="my_modal_1" className="modal">
+                                <div className="modal-box">
+                                    <h3 className="font-bold text-lg">{modalContent?.name}</h3>
+                                    <p className="py-4">{modalContent?.episode}</p>
+                                    <p className="py-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Proin varius ullamcorper lobortis. Sed eget sem risus</p>
+                                    <div className="modal-action">
+                                        <form method="dialog">
+                                            {/* if there is a button in form, it will close the modal */}
+                                            <button className="btn">Close</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </dialog>
                         </div>
                     </div>
                 </div>
